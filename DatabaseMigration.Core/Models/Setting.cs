0bd6c7f4d082e4075e2b1dac5d3ab003283115fc
alien <734329218@qq.com>
2019-04-18 16:14:31 +0800

    Update Setting.cs
    恢复初始配置 DataBatchSize=500

--- DatabaseMigration.Core/Models/Setting.cs
+++ DatabaseMigration.Core/Models/Setting.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DatabaseMigration.Core
+﻿namespace DatabaseMigration.Core
 {
     public class Setting
     {
         public int CommandTimeout { get; set; } = 600;
-        public int DataBatchSize { get; set; } = 10000;
+        public int DataBatchSize { get; set; } = 500;
     }
 }
